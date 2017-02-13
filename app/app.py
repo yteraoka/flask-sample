8be3f7b972a190732b73e948b2b08969f9cc4b65
Yoshinori Teraoka <teraoka.yoshinori@gmail.com>
2017-02-13 23:59:11 +0900

    bump version 0.2

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! (Version: 0.1)\n"
+    return "Hello World! (Version: 0.2)\n"
 
 if __name__ == "__main__":
     app.run()
