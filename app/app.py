509881e8357826b8fb863a232515d543dfba02f3
Yoshinori Teraoka <teraoka.yoshinori@gmail.com>
2017-02-14 23:07:51 +0900

    add hostname in response

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 app = Flask(__name__)
+
+VERSION = 0.3
 
 @app.route("/")
 def hello():
-    return "Hello World! (Version: 0.2)\n"
+    return "Hello World! (Version: {}) [{}]\n".format(VERSION, os.uname()[1])
 
 if __name__ == "__main__":
     app.run()
